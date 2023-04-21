04d8adfc0defa04bee9d7430de1baef2c3148cb4
R3N4T051LV31R4 <130613061+R3N4T051LV31R4@users.noreply.github.com>
2023-04-21 09:55:51 -0300

    Artifact Registry

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,6 +1,6 @@
 resource "google_artifact_registry_repository" "my-repo" {
   location = var.region
-  repository_id = "devopslab-infra-grupo34"
+  repository_id = "lab-devops-cloud-383422"
   description = "Imagens Docker"
   format = "DOCKER"
 }
