a690a5abfb6cba42faeb630dd7b2355755814253
R3N4T051LV31R4 <130613061+R3N4T051LV31R4@users.noreply.github.com>
2023-04-21 09:32:03 -0300

    Artifact Registry

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,16 +1,3 @@
-# Cria uma VM no Google Cloud
-resource "google_compute_instance" "firstvm" {
-  name         = "helloworld"
-  machine_type = "n1-standard-1"
-  zone         = var.zone
-
-  # Defin a Imagem da VM
-  boot_disk {
-    initialize_params {
-      image = "ubuntu-2004-focal-v20230213"
-    }
-  }
-  
 resource "google_artifact_registry_repository" "my-repo" {
   location = var.region
   repository_id = "labdevops"
